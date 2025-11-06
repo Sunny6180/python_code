2fc95b2aa9fda04343d6db73ee50af565355a196
Sunny6180 <siyang727@gmail.com>
2025-11-06 04:00:42 +0000

    Add number decomposition script with user input

---
+++ 11.6.py
@@ -0,0 +1,23 @@
+# print("hello world")
+# name=input("your name?") 
+# age=input("your age?") 
+# score=input("your score?")
+# print("name is",name,"age is",age,"years old","score is",score)
+num=int(input("input a number"))
+a=num//100
+b=num%100//10
+c=b%10
+print("百位",a)
+print("十位",b)
+print("个位",c)
+# print("      * ")
+# # print("   ***")
+# # print("  *****")
+# # print(" *******")
+# # print("*********")
+# # print(" *******")
+# # print("  *****")
+# # print("   ***")
+# # print("    * ")
+
+''''''
