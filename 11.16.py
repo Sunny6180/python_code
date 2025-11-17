9d617f9c176a7a9114fffd7f400f33a2a5b2f198
Sunny6180 <siyang727@gmail.com>
2025-11-17 00:41:23 +0000

    Add files via upload

---
+++ 11.16.py
@@ -0,0 +1,89 @@
+# a=[1,2,3,4,5]
+# total=sum(a)
+# print(total)
+
+# a=[]
+# for i in range(5):
+#     num = int(input("input a number")) 
+#     a.append(num)
+# total = sum(a)
+# print(total)
+
+# a=[1,2,3]
+# b=[2,5,8]
+# c=[3,1,4]
+# max_a=max(a)
+# max_b=max(b)
+# max_c=max(c)
+# print(max_a)
+# print(max_b)
+# print(max_c)
+
+# oldArr=[1,3,4,5,0,0,6,6,0,5,4,7,6,7,0,5]
+# a=oldArr.count(0)
+# print(a)
+
+# arr=[2,8,6,10,1]
+# arr.sort(reverse=True)
+# print(arr)
+
+# arr=[True,True,False,True]
+# for i in range(len(arr)):
+#     arr[i] = not arr[i]
+# print(arr)
+
+# newArr=[]
+# oldArr =[1,0,3,4,2,0,7,0,5]
+# for i in range(len(oldArr)):
+#     if oldArr[i]!=0:
+#         newArr.append(oldArr[i])
+# print(newArr)
+    
+# newArr=[]
+# oldArr=[1,0,3,4,2,0,7,0,5]
+# for num in oldArr:
+#     if num!=0:
+#         newArr.append(num)
+# print(newArr)
+
+# a=[]
+# for i in range(10):
+#     num = int(input("input a number")) 
+#     a.append(num)
+# for num in a:
+#     if num%2==0:
+#         print(num,end=" ")
+
+# salaries=[]
+# print("input staff number")
+# number=int(input("input staff number"))
+# for i in range(number):
+#     salary=float(input("input salary number"))
+#     salaries.append(salary)
+# print(salaries)
+# avg_salary=sum(salaries)/number
+# print(avg_salary)
+# max_salary=max(salaries)
+# min_salary=min(salaries)
+# max_index=salaries.index(max_salary)+1
+# min_index=salaries.index(min_salary)+1
+# print(max_index,min_index)
+
+
+# arr=[0,0,0,0,0]
+# while True:
+#     number=int(input("input a number"))
+#     if 1<=number<=5:
+#        arr[number-1]+=1
+#     elif number==0:
+#         break
+#     else:
+#         print("error")
+# for i in range(5):
+#     print(f"候选人 {i+1}:{arr[i]} 票")
+
+
+
+
+
+
